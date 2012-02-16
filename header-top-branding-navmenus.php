3a796bfcf9edd2cdfdb5d91d6ae692979c75ac02
Russell Fair <rfair404@gmail.com>
2012-02-16 15:17:57 -0500

    rename files

--- header-top-branding-navmenus.php
+++
@@ -1,8 +0,0 @@
-<?php /* add 'descriptions' to the menu function to use the custom walker involving menu item descriptions */ ?>
-<?php if ( function_exists('h5b_menu') ) h5b_menu('header_primary', 'header-primary', 'descriptions');  ?>
-<hgroup id="top-branding">
-<h1 id="site-title"><a href="<?php echo home_url(); ?>" title="<?php _e('home', 'h5b'); ?>"><?php bloginfo('name');?></a></h1>
-<h2 id="site-description"><?php bloginfo('description');?></h2>
-</hgroup>
-<?php if ( function_exists('h5b_menu') ) h5b_menu('header_secondary', 'header-secondary');  ?>
-
